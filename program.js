ecf7eef50cded2a18400582d24bb2ebfb2e5e60a
sokoly <yura.sokol@gmail.com>
2022-10-23 15:09:03 +0300

    Multiplication function

--- program.js
+++ program.js
@@ -3,3 +3,6 @@ let sum = (a,b) => a + b;
 console.log("Sum is: " + sum(3, 5));
 console.log("Sum is: " + sum(4, 9));
 console.log("Sum is: " + sum(1, 10));
+
+let multiplication = (x, y) => x * y;
+console.log ("Myltiplication opp: " + multiplication(8, 4));
