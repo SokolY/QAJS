1c1b7db67729a8db7bc6629edbe560d68a863a26
sokoly <yura.sokol@gmail.com>
2022-10-23 15:00:34 +0300

    FirstCommit

---
+++ Untitled-1.js
@@ -0,0 +1,7 @@
+console.log("Hello World!");
+
+function sum (a, b){
+    return a + b;
+}
+
+console.log(sum(2, 7));
---
+++ program.js
@@ -0,0 +1,4 @@
+let sum = (a,b) => a + b;
+
+console.log("Sum is: " + sum(3, 5));
+console.log("Sum is: " + sum(4, 9));
